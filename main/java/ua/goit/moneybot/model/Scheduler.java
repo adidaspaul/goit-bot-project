84b4b7d0450cad9ead0dee21af0bb49ff0804bac
Paul <quazantip@gmail.com>
2021-12-26 00:27:55 +0400

    First Version that I come up with.

--- main/java/ua/goit/moneybot/model/Scheduler.java
+++ main/java/ua/goit/moneybot/model/Scheduler.java
@@ -1,9 +1,26 @@
 package ua.goit.moneybot.model;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class Scheduler  {
+public class Scheduler {
+
+    UserSettings userSettings = new UserSettings();
+    Facade alert = new Facade();
+    Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            alert.getResponseFromBank(userSettings, Message message);
+        }
+    };
+    public void schedule(TimerTask task,Date notification, long period) {
+        Date notification = new Date();
+        notification.setHour(userSettings.getNotificationTime);
+
+        timer.schedule(task, notification, 84600000);
+    }
 
 }
